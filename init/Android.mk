e52fb4b5b2ce03ef010dc2bed44b13db0843d5b5
hazestrike <johnnytuan17@gmail.com>
2019-07-02 08:53:01 -0700

    Add files via upload

--- init/Android.mk
+++ init/Android.mk
@@ -3,9 +3,15 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init \
+    external/selinux/libselinux/include \
+    external/libcap/libcap/include  
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_draconis.cpp
 LOCAL_MODULE := libinit_draconis
+LOCAL_STATIC_LIBRARIES := \
+    libbase
 
 include $(BUILD_STATIC_LIBRARY)
